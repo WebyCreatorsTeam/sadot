42cdd581bdea01cf0bf9525cab5260d263875ade
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2024-05-27 17:26:29 +0300

    Update Footer.tsx

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -5,6 +5,10 @@ import call from '/icons/call.svg'
 import whatsap from '/icons/whatsap.svg'
 
 const Footer: FC = () => {
+    const text: string = "היי, פניתי אלייך דרך האתר, אשמח לשמוע פרטים נוספים"
+    const sendedText = text.replace(/ /g, "%20")
+    console.log(sendedText)
+
     return (
         <footer>
             <div className='footer-info'>
@@ -13,12 +17,12 @@ const Footer: FC = () => {
                     <img src={path} alt="" width={50} height={50} />
                 </a>
                 <a href="https://ul.waze.com/ul?place=ChIJKXDcnHF_AhURFfkw3bhj5YA&ll=31.42209800%2C34.52438300&navigate=yes&utm_campaign=default&utm_source=waze_website&utm_medium=lm_share_location" target="_blank" aria-label="וויז">
-                    <img src={waze} alt="" width={50} height={50} /> 
+                    <img src={waze} alt="" width={50} height={50} />
                 </a>
             </div>
             <div className='footer-info'>
                 <p className='footer-info__text'>מושב שוקדה</p>
-                <a href='https://wa.me/972506353830' target="_blank" aria-label="וואטסאפ"> {/* ?text=היי%20אשמח%20לפרטים */}
+                <a href={`https://wa.me/972506353830?text=${sendedText}`} target="_blank" aria-label="וואטסאפ"> {/* ?text=היי%20אשמח%20לפרטים */}
                     <img src={whatsap} alt="" width={50} height={50} />
                 </a>
                 <a href="tel:0506353830" aria-label="טלפון">
